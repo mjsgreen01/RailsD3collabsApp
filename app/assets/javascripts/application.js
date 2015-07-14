3f0173384116929a6e9991e60e02677cba31c253
mjsgreen01 <mjsgreen01@gmail.com>
2015-07-14 14:17:49 -0700

    data is in correct format

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,23 +17,50 @@
 
 
 $(document).ready(function() {
-    $("#artists_search input").keyup(function() {
-        console.log($("#artists_search").serialize())
-        $.get($("#artists_search").attr("action"), $("#artists_search").serialize(), function(result){console.log('result is', JSON.stringify(result))}, "json");
+    $("#artists_search").submit(function() {
+        // console.log($("#artists_search").find("#search").val())
+        var searchedArtist = $("#artists_search").find("#search").val();
+        $.get($("#artists_search").attr("action"), $("#artists_search").serialize(), function(result){
+            // console.log('results is ', JSON.stringify(result));
+            var artistCount = {}
+            var d3array = [];
+            for(var i = 0; i < result.length; i++){
+                artist = result[i].name;
+                if(!artistCount[artist]){
+                    artistCount[artist] = 1;
+                }else{
+                    artistCount[artist] += 1;
+                }
+            }
+            // result.each(function(){
+            //     artist = $(this);
+            //     if(!artistCount[artist]){
+            //         artistCount[artist] = 1;
+            //     }else{
+            //         artistCount[artist] += 1;
+            //     }
+            // });
+            for(var i in artistCount){
+                d3array.push({'label' : i, 'value' : artistCount[i]});
+            }
+            console.log('d3 array is ', JSON.stringify(d3array))
+
+
+        }, "json");
         $('body').scrollTop(0);
         return false;
     });
 
-    if ($('.pagination').length) {
-        $(window).scroll(function() {
-            var url = $('.pagination .next_page').attr('href');
-            if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-                $('.pagination').text("Please Wait...");
-                return $.getScript(url);
-            }
-        });
-        return $(window).scroll();
-    }
+    // if ($('.pagination').length) {
+    //     $(window).scroll(function() {
+    //         var url = $('.pagination .next_page').attr('href');
+    //         if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+    //             $('.pagination').text("Please Wait...");
+    //             return $.getScript(url);
+    //         }
+    //     });
+    //     return $(window).scroll();
+    // }
 
 
 });
